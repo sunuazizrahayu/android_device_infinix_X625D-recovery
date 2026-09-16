12396a08e7449b0e2392ead8119e225c7de95bbf
Sunu Aziz Rahayu <sunuazizrahayu@gmail.com>
2026-09-16 15:27:31 +0700

    fix decrypt: include MT6765 keymaster/gatekeeper blobs in recovery ramdisk

--- device.mk
+++ device.mk
@@ -4,3 +4,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := false
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# Blob keymaster/gatekeeper MT6765 untuk decrypt FDE (footer di /metadata).
+# Tanpa ini log error: "could not find any keystore module / Failed to init keymaster"
+# dan "Unable to decrypt with default password" padahal tidak ada PIN.
+# File di recovery/root/ -> recovery ramdisk /vendor/... (tempat libhardware mencari keystore.mt6765.so).
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
